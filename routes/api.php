335e55e776675bef7662e9e9fde338506878eab8
robertPaardekooper <paardekooper.robert@gmail.com>
2021-01-18 01:05:23 +0100

    queue worker

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/', function () {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return view('welcome');
+
+});
+Route::get('check-queue', function(){
+
+  Mail::to('paardekooper.robert@gmail.com')->send(new TestMail());
+
+  return 'Working';
+
 });
