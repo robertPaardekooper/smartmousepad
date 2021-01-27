e88a5489c961fe447e09add98b34aa512e308fb2
robertPaardekooper <paardekooper.robert@gmail.com>
2021-01-27 15:44:36 +0100

    change User.php volgorde

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,15 +8,15 @@ class User extends Model
 {
     protected $table = "user";
 
-    public function getAverageUser(){
-      return $this->hasMany("App\Models\AverageUser", "user_id", "user_id");
+    public function getAverageEventUser(){
+      return $this->hasOne("App\Models\AverageEventUser", "user_id", "user_id");
     }
 
     public function getAverageGroup(){
-      return $this->hasMany("App\Models\AverageGroup", "group_id", "group_id");
+      return $this->hasOne("App\Models\AverageGroup", "group_id", "group_id");
     }
 
-    public function getAverageEventUser(){
-      return $this->hasMany("App\Models\AverageEventUser", "user_id", "user_id");
+    public function getAverageUser(){
+      return $this->hasOne("App\Models\AverageUser", "user_id", "user_id");
     }
 }
