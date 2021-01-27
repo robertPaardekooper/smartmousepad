c622b3713765b256f8a5a5ca4ae4c6d22777a1b3
robertPaardekooper <paardekooper.robert@gmail.com>
2021-01-27 14:29:44 +0100

    make comments

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,9 +9,9 @@ class UserController extends Controller
 {
     public function show($user_id){
       $user = User::where('user_id','=',$user_id)->first();
-      $average_user = $user->getAverageUser;
-      $average_group = $user->getAverageGroup;
-      $average_event_user = $user->getAverageEventUser;
+      // $average_user = $user->getAverageUser;
+      // $average_group = $user->getAverageGroup;
+      // $average_event_user = $user->getAverageEventUser;
       return $user;
     }
 }
