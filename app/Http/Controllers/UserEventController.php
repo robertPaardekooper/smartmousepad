72300e193805aba2d68c560aabbca98c98b98162
robertPaardekooper <paardekooper.robert@gmail.com>
2021-01-29 19:34:21 +0100

    UserEvent POST

--- app/Http/Controllers/UserEventController.php
+++ app/Http/Controllers/UserEventController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserEvent;
 
 class UserEventController extends Controller
 {
-
     public function store(Request $request){
       $user_event = UserEvent::create($request->all());
 
