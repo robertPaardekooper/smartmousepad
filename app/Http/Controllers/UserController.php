1eef0067749e475e233e5c3d164a1b412dec55a0
robertPaardekooper <paardekooper.robert@gmail.com>
2021-01-31 17:58:58 +0100

    first() is veranderd in get()

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
     }
 
     public function show($user_id){
-      $user = User::where('user_id','=',$user_id)->first();
+      $user = User::where('user_id','=',$user_id)->get();
       $average_event_user = $user->getAverageEventUser;
       $average_group = $user->getAverageGroup;
       $average_user = $user->getAverageUser;
